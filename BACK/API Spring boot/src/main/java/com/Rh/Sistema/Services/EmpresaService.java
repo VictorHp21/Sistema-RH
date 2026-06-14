6b2143ad1eda9bdb6a9cadd2d8e5ac9c9c3d2834
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2026-06-14 13:31:10 -0300

    services da classe empresa e respectivas regras de negócio

--- BACK/API Spring boot/src/main/java/com/Rh/Sistema/Services/EmpresaService.java
+++ BACK/API Spring boot/src/main/java/com/Rh/Sistema/Services/EmpresaService.java
@@ -6,6 +6,7 @@ import com.Rh.Sistema.Repositories.EmpresaRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,8 +28,17 @@ public class EmpresaService {
         return empresa.getFuncionarios();
     }
 
+    private List<Funcionario> funcionariosEmpresa(Long empresaId){
+        return buscarEmpresaId(empresaId).get().getFuncionarios();
+    }
+
     public Optional<Empresa>buscarEmpresaId(Long id){
         return repository.findById(id);
+    }
+
+    private Empresa buscarEmpresa(Long empresaId){
+        return repository.findById(empresaId)
+                .orElseThrow(() -> new RuntimeException("Empresa não encontrada"));
     }
 
     public Empresa cadastrarEmpresa(Empresa empresa){
@@ -56,6 +66,8 @@ public class EmpresaService {
     }
 
 
+    // metodo para pegar funcionarios pelo repository
+
 
     // Regras de negócio:
 
@@ -65,18 +77,131 @@ public class EmpresaService {
 
         BigDecimal folhaDePagamento = BigDecimal.valueOf(0.0);
 
-        Empresa empresaExiste = repository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Empresa não encontrada"));
-
-        List<Funcionario> funcionarios = empresaExiste.getFuncionarios();
 
         // bigdecimal é imutavel entao usar add()
 
-        for (Funcionario f: funcionarios){
+        for (Funcionario f: funcionariosEmpresa(id)){
             folhaDePagamento = folhaDePagamento.add(f.getSalario());
         }
 
         return folhaDePagamento;
+
+    }
+
+    public StringBuilder gerarRelatorioFolhaSalarial(Long id){
+
+        Empresa empresaExiste = buscarEmpresa(id);
+
+        StringBuilder relatorio = new StringBuilder();
+
+
+        relatorio.append("\n===== FOLHA SALARIAL =====\n");
+        relatorio.append("Empresa: ").append(empresaExiste.getNome()).append("\n");
+        relatorio.append("CNPJ: ").append(empresaExiste.getCnpj()).append("\n\n");
+
+        BigDecimal totalFolha = BigDecimal.valueOf(0.0);
+
+        for (Funcionario f : funcionariosEmpresa(id)) {
+            relatorio.append("Funcionário: ")
+                    .append(f.getNome())
+                    .append(" | Salário: R$ ")
+                    .append(String.format("%.2f", f.getSalario()))
+                    .append("\n");
+
+            totalFolha = totalFolha.add(f.getSalario());
+        }
+
+        relatorio.append("\n-------------------------\n");
+        relatorio.append("Total da folha salarial: R$ ")
+                .append(String.format("%.2f", totalFolha));
+
+        return relatorio;
+
+    }
+
+
+    // Funcionário com o maior sálario
+    public Funcionario funcionarioMaiorSalario(Long idEmpresa){
+
+        Empresa empresaExiste = buscarEmpresa(idEmpresa);
+
+        if(empresaExiste.getFuncionarios().isEmpty()){
+            throw new RuntimeException("Nenhum funcionário cadastrado.");
+        }
+
+        List<Funcionario> funcionarios = funcionariosEmpresa(idEmpresa);
+
+        BigDecimal maior = funcionarios.get(0).getSalario();
+
+        Funcionario funcionarioMaiorSalario = funcionarios.get(0);
+
+        StringBuilder relatorioMaiorSalarioFuncionario = new StringBuilder("");
+
+        for (Funcionario f: funcionarios){
+
+            //Obs para consulta
+            // comparação com bigdecimal deve ser feita com compareTo() e não operadores como > ou < ou =
+
+            if(f.getSalario().compareTo(maior) == 1){
+                maior = f.getSalario();
+                funcionarioMaiorSalario = f;
+            }
+
+        }
+
+        return funcionarioMaiorSalario;
+
+    }
+
+
+    // Funcionário com o menor sálario
+
+    public Funcionario funcionarioMenorSalario(Long idEmpresa){
+
+        Empresa empresaExiste = buscarEmpresa(idEmpresa);
+
+        if(empresaExiste.getFuncionarios().isEmpty()){
+            throw new RuntimeException("Nenhum funcionário cadastrado.");
+        }
+
+        List<Funcionario> funcionarios = funcionariosEmpresa(idEmpresa);
+
+        BigDecimal menor = funcionarios.get(0).getSalario();
+
+        Funcionario funcionarioMenorSalario = funcionarios.get(0);
+
+        StringBuilder relatorioMenorSalarioFuncionario = new StringBuilder("");
+
+        for (Funcionario f: funcionarios){
+
+            if(f.getSalario().compareTo(menor) == -1){
+                menor = f.getSalario();
+                funcionarioMenorSalario = f;
+            }
+
+        }
+
+        return funcionarioMenorSalario;
+
+    }
+
+    // Media sálarial
+
+    public BigDecimal mediaSalarial(Long idEmpresa){
+        BigDecimal soma = BigDecimal.ZERO;
+        int numFunc = 0;
+
+        for (Funcionario f: funcionariosEmpresa(idEmpresa)){
+            soma = soma.add(f.getSalario());
+            numFunc++;
+        }
+
+        BigDecimal media = soma.divide(
+                BigDecimal.valueOf(numFunc),
+                2,
+                RoundingMode.HALF_UP);
+
+        return media;
 
     }
 
